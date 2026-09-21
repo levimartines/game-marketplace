ccef900ec2518edcdf0afa1877b49c043cf7de9f
levimartines <levi.mferreira@gmail.com>
2026-09-21 09:15:37 -0300

    ci: fix tests not working on pipeline

---
+++ src/test/java/dev/levi/gamemarketplace/AbstractIntegrationTest.java
@@ -0,0 +1,12 @@
+package dev.levi.gamemarketplace;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
+public abstract class AbstractIntegrationTest {
+
+}
--- src/test/java/dev/levi/gamemarketplace/GameMarketplaceApplicationTests.java
+++ src/test/java/dev/levi/gamemarketplace/GameMarketplaceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GameMarketplaceApplicationTests {
+class GameMarketplaceApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
